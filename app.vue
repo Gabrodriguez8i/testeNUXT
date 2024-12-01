66c80bcc80594941c05617362ac76a72705bed6c
gabriel rodriguez <gabrielrodriguez@8i.cl>
2024-11-30 23:34:34 -0400

    se sube supabase

--- app.vue
+++ app.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <button @click="login">Iniciar sesión</button>
   </div>
 </template>
+
+<script setup>
+const { $supabase } = useNuxtApp()
+
+const login = async () => {
+  const { data, error } = await $supabase.auth.signInWithPassword({
+    email: 'gr985809@gmail.com',
+    password: 'abc123,.',
+  })
+
+  if (error) {
+    console.log('Error en login:', error.message)
+  } else {
+    console.log('Usuario logueado:', data)
+  }
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  compatibilityDate: '2024-12-01',
+  devtools: { enabled: true },
+  nitro:{
+    preset: "cloudflare-pages"
+  },
+    modules: [
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/fonts",
+    "@samk-dev/nuxt-vcalendar",
+  ],
+
+// nuxt.config.js
+  runtimeConfig: {
+ 
+      // Variables públicas accesibles en el cliente
+      public: {
+          SUPABASE_URL: process.env.SUPABASE_URL,
+          // Variables privadas solo para el servidor
+          SUPABASE_KEY: process.env.SUPABASE_KEY,
+      }
+  },
+  plugins: [
+      '~/plugins/supabase.js'
+  ],
+
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "es",
+      },
+      title: "Creati Hogar",
+    },
+    pageTransition: { name: "page", mode: "out-in" },
+  }
 })
